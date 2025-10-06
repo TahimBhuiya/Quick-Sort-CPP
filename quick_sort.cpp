8e87ef16529f1548871d265d4693d4ae718ddd5b
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-10-05 20:04:27 -0400

    Added dependencies

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,3 +1,5 @@
 // Quick Sort in C++
 // This program implements the Quick Sort algorithm to sort an array in ascending order.
 // It uses the divide-and-conquer approach by partitioning the array around a pivot element.
+#include <iostream>
+using namespace std;
